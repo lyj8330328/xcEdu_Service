c1875fe5124d8761c5c8d45e0dd8a9b95da79489
lyj8330328 <980500807@qq.com>
2019-03-19 22:18:15 +0800

    代码优化

--- xc-framework-common/src/main/java/com/xuecheng/framework/model/response/CommonCode.java
+++ xc-framework-common/src/main/java/com/xuecheng/framework/model/response/CommonCode.java
@@ -3,28 +3,38 @@ package com.xuecheng.framework.model.response;
 import lombok.ToString;
 
 /**
- * @Author: mrt.
- * @Description:
- * @Date:Created in 2018/1/24 18:33.
+ * @Author: 98050
  * @Modified By:
  */
 
 @ToString
 public enum CommonCode implements ResultCode{
-
+    /**
+     * 操作成功
+     */
     SUCCESS(true,10000,"操作成功！"),
+    /**
+     * 操作失败
+     */
     FAIL(false,11111,"操作失败！"),
     UNAUTHENTICATED(false,10001,"此操作需要登陆系统！"),
     UNAUTHORISE(false,10002,"权限不足，无权操作！"),
     SERVER_ERROR(false,99999,"抱歉，系统繁忙，请稍后重试！");
 //    private static ImmutableMap<Integer, CommonCode> codes ;
-    //操作是否成功
+    /**
+     * 操作是否成功
+     */
     boolean success;
-    //操作代码
+    /**
+     * 操作代码
+     */
     int code;
-    //提示信息
+    /**
+     * 提示信息
+     */
     String message;
-    private CommonCode(boolean success,int code, String message){
+
+    CommonCode(boolean success,int code, String message){
         this.success = success;
         this.code = code;
         this.message = message;
--- xc-framework-common/src/main/java/com/xuecheng/framework/model/response/QueryResult.java
+++ xc-framework-common/src/main/java/com/xuecheng/framework/model/response/QueryResult.java
@@ -6,16 +6,17 @@ import lombok.ToString;
 import java.util.List;
 
 /**
- * @Author: mrt.
- * @Description:
- * @Date:Created in 2018/1/24 18:33.
- * @Modified By:
+ * @Author: 98050
  */
 @Data
 @ToString
 public class QueryResult<T> {
-    //数据列表
+    /**
+     * 数据列表
+     */
     private List<T> list;
-    //数据总数
+    /**
+     * 数据总数
+     */
     private long total;
 }
