84d554ab6ba5f800cee2117c84102733d2f100ac
lyj8330328 <980500807@qq.com>
2019-07-05 10:14:52 +0800

    mp4转m3u8

--- xc-service-manage-media-processor/src/main/java/com/xuecheng/manage/media/process/mq/MediaProcessTask.java
+++ xc-service-manage-media-processor/src/main/java/com/xuecheng/manage/media/process/mq/MediaProcessTask.java
@@ -3,6 +3,7 @@ package com.xuecheng.manage.media.process.mq;
 import com.alibaba.fastjson.JSON;
 import com.xuecheng.framework.domain.media.MediaFile;
 import com.xuecheng.framework.domain.media.MediaFileProcessm3u8;
+import com.xuecheng.framework.utils.HlsVideoUtil;
 import com.xuecheng.framework.utils.Mp4VideoUtil;
 import com.xuecheng.manage.media.process.dao.MediaFileRepository;
 import org.apache.logging.log4j.Logger;
@@ -12,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -77,12 +79,47 @@ public class MediaProcessTask {
         if (result == null || !result.equals("success")){
             //5.操作失败写入处理日志
             //处理状态设置为处理失败
-            mediaFile.setProcessStatus("303003");
-            MediaFileProcessm3u8 mediaFileProcessm3u8 = new MediaFileProcessm3u8();
-            mediaFileProcessm3u8.setErrormsg(result);
-            mediaFile.setMediaFileProcessm3u8(mediaFileProcessm3u8);
-            mediaFileRepository.save(mediaFile);
+            processFail(mediaFile,mediaFileRepository, result,"303003");
             return;
         }
+        //5.生成m3u8
+        //mp4视频地址
+        videoPath = serverPath + mediaFile.getFilePath() + videoName;
+        String m3u8Name = mediaFile.getFileId() + ".m3u8";
+        String m3u8FolderPath = serverPath + mediaFile.getFilePath() + "hls/";
+        HlsVideoUtil hlsVideoUtil = new HlsVideoUtil(ffmpegPath, videoPath, m3u8Name, m3u8FolderPath);
+        result = hlsVideoUtil.generateM3u8();
+        if (result == null || !result.equals("success")){
+            //操作失败写入处理日志
+            processFail(mediaFile,mediaFileRepository,result,"303003");
+            return;
+        }
+        //6.获取m3u8列表
+        List<String> tsList = hlsVideoUtil.get_ts_list();
+        //7.更新处理状态为成功
+        mediaFile.setProcessStatus("303002");
+        MediaFileProcessm3u8 mediaFileProcessm3u8 = new MediaFileProcessm3u8();
+        mediaFileProcessm3u8.setTslist(tsList);
+        mediaFile.setMediaFileProcessm3u8(mediaFileProcessm3u8);
+        //8.设置m3u8文件url
+        mediaFile.setFileUrl(mediaFile.getFilePath() + "hls/" + m3u8Name);
+        mediaFileRepository.save(mediaFile);
     }
+
+    /**
+     * 处理失败
+     * @param mediaFile 媒体资源文件
+     * @param mediaFileRepository
+     * @param status 状态
+     * @param result
+     */
+    private void processFail(MediaFile mediaFile, MediaFileRepository mediaFileRepository, String status, String result) {
+        mediaFile.setProcessStatus(status);
+        MediaFileProcessm3u8 mediaFileProcessm3u8 = new MediaFileProcessm3u8();
+        mediaFileProcessm3u8.setErrormsg(result);
+        mediaFile.setMediaFileProcessm3u8(mediaFileProcessm3u8);
+        mediaFileRepository.save(mediaFile);
+    }
+
+
 }
